c5517d3bfa2ad700c528658b5871f3e49c09b0d7
rhit-rykerzhang <rykerzhang048109@gmail.com>
2022-12-11 20:29:41 -0500

    part4 and 5

--- FollowAlongs/dart_playground/dart_playgrounds/lib/Part4_functions.dart
+++ FollowAlongs/dart_playground/dart_playgrounds/lib/Part4_functions.dart
@@ -3,6 +3,11 @@ void main() {
   print("addTwoNumbers(2,3) ${addTwoNumbers(2, 3)}");
   print("addTwoNumbersWithTPES(2,3) ${addTwoNumberWithTypes(2, 3)}");
   print("addTwoNumbersShorter(2,3) ${addTwoNumbersShorter(2, 3)}");
+  print(
+      "divideWithNamedParameters{dividend: 7} ${divideWithNamedParameters(dividend: 7)}");
+  print(
+      "divideWithNamedParameters{dividend: 7} ${divideWithNamedParameters(dividend: 7, divisor: 2)}");
+  //print(mixedSet{2, dividend: 8, divisor: 3});
 }
 
 int addTwoNumbers(a, b) {
@@ -14,3 +19,15 @@ int addTwoNumberWithTypes(int a, int b) {
 }
 
 int addTwoNumbersShorter(a, b) => a + b;
+
+int divideWithNamedParameters({required int dividend, int divisor = 1}) {
+  return dividend ~/ divisor;
+}
+
+int mixed(int multiplier, {required int dividend, int divisor = 1}) {
+  return multiplier * dividend ~/ divisor;
+}
+
+int notUsedByFlutter(int a, {int b = 0, int c = 0}) {
+  return a + b + c;
+}
---
+++ FollowAlongs/dart_playground/dart_playgrounds/lib/Part5_classes.dart
@@ -0,0 +1,23 @@
+void main() {
+  print("Classes");
+  var pt = DoublePoint(2, 5);
+  print("pt = ${pt.x} ${pt.y} $pt");
+}
+
+//Class DoublePoint
+// var pt = DoublePoint(2,5);
+
+class DoublePoint {
+  late double x;
+  late double y;
+
+  DoublePoint(x, y) {
+    this.x = x * 2.0;
+    this.y = y * 2.0;
+  }
+
+  @override
+  String toString() {
+    return "(${this.x}, ${this.y})";
+  }
+}
