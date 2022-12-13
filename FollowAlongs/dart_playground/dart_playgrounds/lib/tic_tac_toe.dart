0ce80e4c1e80339f446b1a26c3db289143d27724
rhit-rykerzhang <rykerzhang048109@gmail.com>
2022-12-13 13:49:17 -0500

    12/13/2022

---
+++ FollowAlongs/dart_playground/dart_playgrounds/lib/tic_tac_toe.dart
@@ -0,0 +1,83 @@
+enum TicTacToeMark {
+  x,
+  o,
+  none,
+}
+
+enum TicTacToeState {
+  xTurn,
+  oTurn,
+  xWon,
+  oWon,
+  tie,
+}
+
+class MyTicTacToeGame {
+  var board;
+  var turn;
+  MyTicTacToeGame() {
+    turn = TicTacToeState.xTurn;
+    board = new List.filled(
+        3, [TicTacToeMark.none, TicTacToeMark.none, TicTacToeMark.none]);
+  }
+
+  void xMove(int placeNum) {
+    int row = (placeNum / 9).floor();
+    int column = placeNum - row * 3;
+    board[row][column] = TicTacToeMark.x;
+    checkGame();
+  }
+
+  void oMove(int placeNum) {
+    int row = (placeNum / 9).floor();
+    int column = placeNum - row * 3;
+    board[row][column] = TicTacToeMark.o;
+    checkGame();
+  }
+
+  TicTacToeState checkGame() {
+    var boardT = new List.filled(
+        3, [TicTacToeMark.none, TicTacToeMark.none, TicTacToeMark.none]);
+    var boardD = new List.filled(
+        2, [TicTacToeMark.none, TicTacToeMark.none, TicTacToeMark.none]);
+    //make row list: just the board
+
+    //make column list
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        boardT[i][j] = board[j][i];
+      }
+      //make diagonal list
+      boardD[0] = [board[0][0], board[1][1], board[2][2]];
+      boardD[1] = [board[0][2], board[1][1], board[2][0]];
+
+      //check row:
+      if (board.contains([TicTacToeMark.x, TicTacToeMark.x, TicTacToeMark.x])) {
+        turn = TicTacToeState.xWon;
+      } else if (board
+          .contains([TicTacToeMark.o, TicTacToeMark.o, TicTacToeMark.o])) {
+        turn = TicTacToeState.oWon;
+      } else if (boardT
+          .contains([TicTacToeMark.x, TicTacToeMark.x, TicTacToeMark.x])) {
+        turn = TicTacToeState.xWon;
+      } else if (boardT
+          .contains([TicTacToeMark.o, TicTacToeMark.o, TicTacToeMark.o])) {
+        turn = TicTacToeState.oWon;
+      } else if (boardD
+          .contains([TicTacToeMark.x, TicTacToeMark.x, TicTacToeMark.x])) {
+        turn = TicTacToeState.xWon;
+      } else if (boardD
+          .contains([TicTacToeMark.o, TicTacToeMark.o, TicTacToeMark.o])) {
+        turn = TicTacToeState.oWon;
+      }
+    }
+  }
+}
+
+void main() {
+  print("testing tic tac toe game");
+  //Make a game object
+  //Press the middle square
+  //Somehow print or test if it worked
+  //press another spot
+}
---
+++ FollowAlongs/dart_playground/dart_playgrounds/lib/tic_tac_toe_correct.dart
@@ -0,0 +1,110 @@
+import 'package:test/test.dart';
+
+enum TicTacToeMark {
+  x,
+  o,
+  none,
+}
+
+enum TicTacToeState {
+  xTurn,
+  oTurn,
+  xWon,
+  oWon,
+  tie,
+}
+
+class TicTacToeGame {
+  TicTacToeState state = TicTacToeState.xTurn;
+  final List<TicTacToeMark> board =
+      List<TicTacToeMark>.filled(9, TicTacToeMark.none);
+
+  void pressedSquare(int index) {
+    if (board[index] != TicTacToeMark.none) {
+      print("This spot is not empty.");
+    }
+
+    if (state == TicTacToeState.xTurn) {
+      board[index] = TicTacToeMark.x;
+      state = TicTacToeState.oTurn;
+      checkForGameOver();
+    } else if (state == TicTacToeState.oTurn) {
+      board[index] = TicTacToeMark.o;
+      state = TicTacToeState.xTurn;
+      checkForGameOver();
+    } else {
+      print('Game over. Ignore the press');
+    }
+  }
+
+  void checkForGameOver() {
+    if (!board.contains(TicTacToeMark.none)) {
+      state = TicTacToeState.tie;
+    }
+    final linesOf3 = <String>[];
+    final bs = boardString;
+    linesOf3.add(bs[0] + bs[1] + bs[2]);
+    linesOf3.add(bs[3] + bs[4] + bs[5]);
+    linesOf3.add(bs[6] + bs[7] + bs[8]);
+    linesOf3.add(bs[0] + bs[3] + bs[6]);
+    linesOf3.add(bs[1] + bs[4] + bs[7]);
+    linesOf3.add(bs[2] + bs[5] + bs[8]);
+    linesOf3.add(bs[0] + bs[4] + bs[8]);
+    linesOf3.add(bs[6] + bs[4] + bs[2]);
+    for (final lineOf3 in linesOf3) {
+      if (lineOf3 == "XXX") {
+        state = TicTacToeState.xWon;
+      } else if (lineOf3 == "OOO") {
+        state = TicTacToeState.oWon;
+      }
+    }
+  }
+
+  String get stateString => state.toString().split('.').last;
+
+  String get boardString {
+    String b = '';
+    for (final mark in board) {
+      b += (mark == TicTacToeMark.x)
+          ? "X"
+          : ((mark == TicTacToeMark.o) ? "O" : "-");
+    }
+    return b;
+  }
+
+  @override
+  String toString() {
+    return '$stateString $boardString';
+  }
+}
+
+void devUnitTest() {
+  var game = TicTacToeGame();
+  test('String.split() splits the string on the delimiter', () {
+    expect(game.state, equals(TicTacToeState.xTurn));
+    expect(game.board.length, equals(9));
+    expect(game.board[0], equals(TicTacToeMark.none));
+    expect(game.board[4], equals(TicTacToeMark.none));
+  });
+
+  test('Single X Press', () {
+    game.pressedSquare(4);
+    expect(game.state, equals(TicTacToeState.xTurn));
+  });
+}
+
+void main() {
+  print('Testing Tictactoe game');
+  devUnitTest();
+}
+
+void devWithPrint() {
+  var game = TicTacToeGame();
+  //print(game.state);
+  //print(game.board);
+  print('xTurn ---------- $game');
+  game.pressedSquare(4);
+  print("OTurn ----X---- $game");
+  game.pressedSquare(0);
+  print("Xturn O---X---- $game");
+}
